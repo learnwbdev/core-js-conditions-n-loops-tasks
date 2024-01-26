f56fd94df5bf7a65aec417f641f97dee3a49f051
Alexandra Paramonova <alpar.wb@gmail.com>
2024-01-26 23:22:55 +0500

    feat: implement canQueenCaptureKing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -74,8 +74,18 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  let isOnStraightLine;
+  if (queen.x === king.x || queen.y === king.y) {
+    isOnStraightLine = true;
+  } else if (king.x - queen.x === king.y - queen.y) {
+    isOnStraightLine = true;
+  } else if (king.x - queen.x === -(king.y - queen.y)) {
+    isOnStraightLine = true;
+  } else {
+    isOnStraightLine = false;
+  }
+  return isOnStraightLine;
 }
 
 /**
